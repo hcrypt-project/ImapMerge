a0cf8c0934a46d516e460172c6d37b4818b71bc2
Michael Brenner <info@hcrypt.com>
2014-01-05 15:01:49 +0100

    starttls: negotiation proxy->server

--- imapmerged.c
+++ imapmerged.c
@@ -5,9 +5,266 @@
  *      Author: micha
  */
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
 
-int main(int argc, char **argv)
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <openssl/pem.h>
+
+int checkMulti(char *s);
+int readLine(int sockfd,char *buf,int size);
+char *stripCrlf(char *s);
+
+int main(int argc, char *argv[])
 {
+    int listenfd = 0, clientfd = 0;
+    int logout=0,multi=0,starttls=0,tlsmode=0;
+	int serverfd = 0, n = 0;
 
+	struct sockaddr_in serv_addr;
+	struct sockaddr_in imap_addr;
+
+	char recvBuff[1024];
+    char sendBuff[1025];
+
+    const SSL_METHOD *method;
+    SSL_CTX *ctx;
+    SSL *ssl;
+
+    puts("imapmerged: starting.");
+
+    puts("imapmerged: setting up openssl.");
+    OpenSSL_add_all_algorithms();
+    ERR_load_BIO_strings();
+    ERR_load_crypto_strings();
+    SSL_load_error_strings();
+
+    if(SSL_library_init() < 0)
+    {
+    	puts("imapmerged: openssl init failed.");
+    	return -1;
+    }
+    method = SSLv23_client_method();
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    memset(sendBuff, '0', sizeof(sendBuff));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(1143);
+
+    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+
+    listen(listenfd, 10);
+
+    puts("imapmerged: proxy ready.");
+
+    while(1)
+    {
+        clientfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+
+        puts("imapmerged: client connected.");
+        ////
+		memset(recvBuff, '0',sizeof(recvBuff));
+		if((serverfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		{
+			printf("imapmerged: could not create socket \n");
+			return 1;
+		}
+
+		memset(&imap_addr, '0', sizeof(imap_addr));
+
+		imap_addr.sin_family = AF_INET;
+		imap_addr.sin_port = htons(143);
+
+		// rrzn "130.75.6.238"
+		// schlund "212.227.15.188"
+		//o2online "91.136.8.190"
+		if(inet_pton(AF_INET, "91.136.8.190", &imap_addr.sin_addr)<=0)
+		{
+			printf("imapmerged: inet_pton error occured\n");
+			return 1;
+		}
+
+		if( connect(serverfd, (struct sockaddr *)&imap_addr, sizeof(imap_addr)) < 0)
+		{
+		   printf("imapmerged: connect failed \n");
+		   return 1;
+		}
+
+		puts("imapmerged: connected to remote IMAP server.");
+
+		// read server info message
+		if( (n = readLine(serverfd, recvBuff, sizeof(recvBuff)-1)) > 0)
+		{
+			//recvBuff[n] = 0;
+			printf("imapmerged: server string(%d)=%s\n",n,stripCrlf(recvBuff));
+
+		}
+		else if(n<0)
+		{
+			printf("imapmerged: server string read error \n");
+			return -1;
+		}
+
+		// forward server info message
+		write(clientfd, recvBuff, n);
+		puts("imapmerged: server string delivered.");fflush(stdout);
+
+		logout=0;
+		starttls=0;
+		tlsmode=0;
+
+		while(!logout)
+		{
+			// read client command
+			if ( (n = readLine(clientfd, recvBuff, sizeof(recvBuff)-1)) > 0)
+			{
+				//recvBuff[n] = 0;
+				printf("imapmerged: client='%s'\n",stripCrlf(recvBuff));
+			}
+			else if(n<0)
+			{
+				printf("imapmerged: read error \n");
+				return -1;
+			}
+
+			if(strstr(recvBuff," LOGOUT"))
+			{
+				logout=1;
+				puts("imapmerged: LOGOUT requested.");
+			}
+
+			if(strstr(recvBuff," STARTTLS"))
+			{
+				starttls=1;
+				//logout=1;
+				puts("imapmerged: STARTTLS requested.");
+			}
+			/////
+			write(serverfd, recvBuff, n);
+
+			puts("imapmerged: start multiline processing.");
+			multi=1;
+			while(multi)
+			{
+				// read client command
+				if ( (n = readLine(serverfd, recvBuff, sizeof(recvBuff)-1)) > 0)
+				{
+					//recvBuff[n] = 0;
+					printf("imapmerged: server='%s'\n",stripCrlf(recvBuff));
+					write(clientfd, recvBuff, n);
+					if(*recvBuff!='*')
+						multi=0;
+				}
+				else
+				{
+					printf("imapmerged: read error \n");
+					return -1;
+				}
+				/////
+
+			}
+			puts("imapmerged: server message processed.");fflush(stdout);
+
+			if(starttls)
+			{
+				printf("imapmerged: starting TLS\n");
+
+				if ( (ctx = SSL_CTX_new(method)) == NULL)
+				{
+					puts("imapmerged: ssl context failed.");
+					return -1;
+				}
+				//SSL_CTX_set_options(ctx, SSL_OP_NO_SSLv2);
+
+				ssl = SSL_new(ctx);
+
+				SSL_set_fd(ssl, serverfd);
+
+				if ( SSL_connect(ssl) != 1 )
+					puts("imapmerged: TLS negotiation failed.");
+				else
+					puts("imapmerged: TLS negotiation successful.");
+
+				tlsmode=1;
+				//logout=1;
+			}
+
+		}
+
+		close(clientfd);
+		puts("imapmerged: client connection closed.");
+		close(serverfd);
+		puts("imapmerged: remote IMAP connection closed.");
+		SSL_free(ssl);
+		SSL_CTX_free(ctx);
+		fflush(stdout);
+     }
+}
+
+int checkMulti(char *s)
+{
+	int i,c;
+
+	i=strlen(s)-1;
+	c=2;
+	while(i>=0&&c>0)
+	{
+		if(s[i]=='\n')
+			c--;
+		i--;
+	}
+	printf("check: %02x\n",s[i+1]);
+	return s[i+1]=='*';
+
+}
+
+int readLine(int sockfd,char *buf,int size)
+{
+	int r=0,n=0;
+
+	while(r<size)
+	{
+		if ( (n = read(sockfd, buf+r, 1)) > 0)
+		{
+			r++;
+			if(r>1)
+			{
+				if(!memcmp(buf+r-2,"\r\n",2))
+				{
+					buf[r]=0;
+					return r;
+				}
+			}
+		}
+	}
+	return -1;
+}
+
+char *stripCrlf(char *s)
+{
+	static char stripBuf[4096];
+	char *c;
+
+	if(strlen(s)>sizeof(stripBuf))
+		strcpy(stripBuf,"(null)");
+	else
+	{
+		strcpy(stripBuf,s);
+		if(c=(char*)strstr(stripBuf,"\r\n"))
+			*c=0;
+	}
+	return stripBuf;
 }
